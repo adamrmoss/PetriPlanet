10e0ca88c10b825865f5c208e7e6c04c441ba095
Adam Moss <adamrmoss@gmail.com>
2014-01-06 23:30:48 -0500

    Arrays.WrapCopy()

---
+++ Source/PetriPlanet.Core/Arrays.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetriPlanet.Core
+{
+  public static class Arrays
+  {
+    public static void WrapCopy<T>(T[] sourceArray, T[] destinationArray, int length)
+    {
+      WrapCopy(sourceArray, (ushort) sourceArray.Length, destinationArray, (ushort) destinationArray.Length, length);
+    }
+
+    public static void WrapCopy<T>(T[] sourceArray, int sourceIndex, T[] destinationArray, int destinationIndex, int length)
+    {
+      var tempArray = new T[length];
+      var firstSourceSegmentLength = Math.Min(sourceArray.Length - sourceIndex, length);
+      Array.Copy(sourceArray, sourceIndex, tempArray, 0, firstSourceSegmentLength);
+      Array.Copy(sourceArray, 0, tempArray, firstSourceSegmentLength, length - firstSourceSegmentLength);
+
+      var firstDestinationSegmentLength = Math.Min(destinationArray.Length - destinationIndex, length);
+      Array.Copy(tempArray, 0, destinationArray, destinationIndex, firstDestinationSegmentLength);
+      Array.Copy(tempArray, firstDestinationSegmentLength, destinationArray, 0, length - firstDestinationSegmentLength);
+    }
+  }
+}
---
+++ Source/PetriPlanet.Specs/ArraysSpecs.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PetriPlanet.Core;
+using PetriPlanet.Specs.Bdd;
+
+namespace PetriPlanet.Specs
+{
+  [TestFixture]
+  public class ArraysSpecs : BddBase
+  {
+    private int[] sourceArray;
+    private int[] destinationArray;
+
+    [SetUp]
+    public void SetUp()
+    {
+      this.sourceArray = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+      this.destinationArray = new[] { 104, 103, 102, 101, 100 };
+    }
+
+    [Test]
+    public void ItCanPerformAnOrdinaryCopy()
+    {
+      Arrays.WrapCopy(this.sourceArray, 3, this.destinationArray, 1, 3);
+
+      Expect(this.destinationArray, EqualTo(new[] {104, 4, 5, 6, 100}));
+    }
+
+    [Test]
+    public void ItCanPerformAWrappedCopy()
+    {
+      Arrays.WrapCopy(this.sourceArray, 8, this.destinationArray, 3, 3);
+
+      Expect(this.destinationArray, EqualTo(new[] {1, 103, 102, 9, 10}));
+    }
+  }
+}
