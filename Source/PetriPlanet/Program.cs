7a3a140c05b6975cc4c23c2c523ee04de09fda32
Adam Moss <adamrmoss@gmail.com>
2014-01-16 13:53:42 -0500

    Load experiment from open file dialog

--- Source/PetriPlanet/Program.cs
+++ Source/PetriPlanet/Program.cs
@@ -13,16 +13,26 @@ namespace PetriPlanet
     [STAThread]
     internal static void Main()
     {
-      var lookupTable = Primes.LookupTable;
-
-      var json = File.ReadAllText(@"ExperimentSetup.json");
-      var experimentSetup = JsonConvert.DeserializeObject<ExperimentSetup>(json);
-      var experiment = new Experiment(experimentSetup);
-
       Application.EnableVisualStyles();
       Application.SetCompatibleTextRenderingDefault(false);
 
+      var lookupTable = Primes.LookupTable;
+
+      var openFileDialog = new OpenFileDialog {
+        Title = "Open Experiment",
+        Filter = "Experiment files (*.experiment)|*.experiment",
+        RestoreDirectory = false,
+      };
+
+      var dialogResult = openFileDialog.ShowDialog();
+      var experimentFilePath = openFileDialog.FileName;
+      var json = File.ReadAllText(experimentFilePath);
+      var experimentSetup = JsonConvert.DeserializeObject<ExperimentSetup>(json);
+      var experiment = new Experiment(experimentSetup);
+
       var experimentForm = new ExperimentForm(experiment);
+      experimentForm.Show();
+      experimentForm.Focus();
       Application.Run(experimentForm);
     }
   }
